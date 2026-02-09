6932136f2e705a80d69a4e6b8068cfa508fa8660
ASAPRATAJ <bartosz.ratajewski@gmail.com>
2026-02-09 21:57:50 +0100

    Fix db.py ENV

--- app/db.py
+++ app/db.py
@@ -1,11 +1,26 @@
 import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+load_dotenv()  # lokalnie ładuje .env
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
+
+DATABASE_URL = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?charset={DB_CHARSET}"
+)
 
 engine = create_engine(
     DATABASE_URL,
+    pool_size=10,
+    max_overflow=20,
+    pool_recycle=280,
     pool_pre_ping=True,
-    pool_size=5,
-    max_overflow=10,
 )
